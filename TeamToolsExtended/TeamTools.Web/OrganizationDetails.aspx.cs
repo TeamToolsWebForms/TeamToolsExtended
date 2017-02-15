3911ee5e2a97d198dc8cdf78944c45bc7b680642
Warez <vlado547@outlook.com>
2017-02-16 00:32:45 +0200

    added error pages

---
+++ TeamToolsExtended/TeamTools.Web/OrganizationDetails.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Web.UI;
+
+namespace TeamTools.Web
+{
+    public partial class OrganizationDetails : Page
+    {
+        private const string RedirectUrl = "~/Profile/MyOrganizations";
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (this.Request.Params["id"] == null)
+            {
+                this.Response.Redirect(RedirectUrl);
+            }
+            else
+            {
+                try
+                {
+                    int paramId = int.Parse(this.Request.Params["id"]);
+                }
+                catch (FormatException)
+                {
+                    this.Response.Redirect(RedirectUrl);
+                }
+            }
+        }
+    }
+}
