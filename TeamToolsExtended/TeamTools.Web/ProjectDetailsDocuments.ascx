fee17078f9bb2f1ad5165e56ef7f95cc99efca99
Warez <vlado547@outlook.com>
2017-02-19 00:25:07 +0200

    project details documents refactoring

--- TeamToolsExtended/TeamTools.Web/ProjectDetailsDocuments.ascx
+++ TeamToolsExtended/TeamTools.Web/ProjectDetailsDocuments.ascx
@@ -1,1 +1,41 @@
 ﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="ProjectDetailsDocuments.ascx.cs" Inherits="TeamTools.Web.ProjectDetailsDocuments" %>
+
+<asp:Panel runat="server" DefaultButton="SearchDocuments" CssClass="container">
+    <div class="row">
+        <div class="dual-list list-right col-md-6">
+            <div>
+                <asp:TextBox runat="server" ClientIDMode="Static" ID="searchDocumentsPattern" CssClass="form-control mb-2 mr-sm-2 mb-sm-0" placeholder="search" />
+                <asp:Button runat="server" Text="Search" CssClass="btn btn-default selector" ID="SearchDocuments" OnClick="SearchDocuments_Click" />
+            </div>
+            <ul>
+                <asp:UpdatePanel runat="server">
+                    <ContentTemplate>
+                        <asp:GridView runat="server" ClientIDMode="Static" ID="ProjectDocuments"
+                            DataKeyNames="Id"
+                            ItemType="TeamTools.Logic.DTO.ProjectDocumentDTO"
+                            CssClass="table table-striped table-bordered table-list"
+                            AutoGenerateColumns="false" AllowPaging="true"
+                            PageSize="20"
+                            OnPageIndexChanging="ProjectDocuments_PageIndexChanging"
+                            SelectMethod="ProjectDocuments_GetData">
+                            <EmptyDataTemplate>
+                                <h3 class="text-center">There are no files yet!</h3>
+                            </EmptyDataTemplate>
+                            <Columns>
+                                <asp:BoundField HeaderStyle-CssClass="text-center" ItemStyle-CssClass="text-center" DataField="FileName" HeaderText="File" />
+                                <asp:TemplateField ItemStyle-CssClass="text-center">
+                                    <ItemTemplate>
+                                        <asp:Button Text="Download" AccessKey="<%# Item.Id %>" CssClass="btn btn-default" ID="DownloadBtn" OnClick="DownloadBtn_Click" runat="server" />
+                                    </ItemTemplate>
+                                </asp:TemplateField>
+                            </Columns>
+                        </asp:GridView>
+                    </ContentTemplate>
+                    <Triggers>
+                        <asp:PostBackTrigger ControlID="ProjectDocuments" />
+                    </Triggers>
+                </asp:UpdatePanel>
+            </ul>
+        </div>
+    </div>
+</asp:Panel>
--- TeamToolsExtended/TeamTools.Web/ProjectDetailsDocuments.ascx.cs
+++ TeamToolsExtended/TeamTools.Web/ProjectDetailsDocuments.ascx.cs
@@ -1,17 +1,82 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Web;
-using System.Web.UI;
 using System.Web.UI.WebControls;
+using TeamTools.Logic.DTO;
+using TeamTools.Logic.Mvp.Profile.MyProjectDetails;
+using TeamTools.Logic.Mvp.Profile.MyProjectDetails.Contracts;
+using WebFormsMvp;
+using WebFormsMvp.Web;
 
 namespace TeamTools.Web
 {
-    public partial class ProjectDetailsDocuments : System.Web.UI.UserControl
+    [PresenterBinding(typeof(ProjectDocumentsPresenter))]
+    public partial class ProjectDetailsDocuments : MvpUserControl<ProjectDetailsViewModel>, IMyProjectDocumentsView
     {
+        private const string RedirectUrl = "~/organizations.aspx";
+        private int projectId;
+
+        public event EventHandler<ProjectDetailsEventArgs> GetDocuments;
+        public event EventHandler<ProjectDetailsEventArgs> GetDocumentInfo;
+        public event EventHandler<ProjectDocumentsEventArgs> FindDocuments;
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (this.Request.Params["id"] == null)
+            {
+                this.Response.Redirect(RedirectUrl);
+            }
+            else
+            {
+                try
+                {
+                    int paramId = int.Parse(this.Request.Params["id"]);
+                    this.projectId = paramId;
+                    this.GetDocuments?.Invoke(sender, new ProjectDetailsEventArgs(this.projectId));
+                }
+                catch (FormatException)
+                {
+                    this.Response.Redirect(RedirectUrl);
+                }
+            }
+        }
 
+        protected void SearchDocuments_Click(object sender, EventArgs e)
+        {
+            string pattern = this.searchDocumentsPattern.Text;
+
+            this.FindDocuments?.Invoke(sender, new ProjectDocumentsEventArgs(this.projectId, pattern));
+
+            this.ProjectDocuments.DataBind();
+        }
+
+        protected void ProjectDocuments_PageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+            this.ProjectDocuments.PageIndex = e.NewPageIndex;
+            this.ProjectDocuments.DataBind();
+        }
+        
+        public IQueryable<ProjectDocumentDTO> ProjectDocuments_GetData()
+        {
+            return this.Model.Project.ProjectDocuments.OrderBy(x => x.Id).AsQueryable();
+        }
+
+        protected void DownloadBtn_Click(object sender, EventArgs e)
+        {
+            Button downloadBtn = (Button)sender;
+            try
+            {
+                int documentId = int.Parse(downloadBtn.AccessKey);
+                this.GetDocumentInfo?.Invoke(sender, new ProjectDetailsEventArgs(documentId));
+
+                this.Response.ContentType = this.Model.DocumentInfo.FileExtension;
+                this.Response.AddHeader("Content-Disposition", $"attachment;filename={this.Model.DocumentInfo.FileName}");
+                this.Response.BinaryWrite(this.Model.DocumentInfo.FileContent);
+                this.Response.End();
+            }
+            catch (FormatException)
+            {
+                // script error
+            }
         }
     }
 }
