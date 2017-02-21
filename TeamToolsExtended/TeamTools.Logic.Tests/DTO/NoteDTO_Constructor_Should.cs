5abee0a66f3207bc72b7d2aef5e2acf65713d95a
VelinYordanov <velin.iordanov@gmail.com>
2017-02-21 16:59:29 +0200

    Addded unit tests for a part of the logic project.

---
+++ TeamToolsExtended/TeamTools.Logic.Tests/DTO/NoteDTO_Constructor_Should.cs
@@ -0,0 +1,58 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TeamTools.Logic.DTO;
+
+namespace TeamTools.Logic.Tests.DTO
+{
+    [TestFixture]
+    public class NoteDTOConstructor_Should
+    {
+        [Test]
+        public void CreateAnObjectOfTheSameType()
+        {
+            var content = "content";
+            var title = "title";
+            var userId = "id";
+            var note = new NoteDTO(title, content, userId);
+
+            Assert.IsInstanceOf<NoteDTO>(note);
+        }
+
+        [Test]
+        public void SetTitleCorrectly()
+        {
+            var content = "content";
+            var title = "title";
+            var userId = "id";
+            var note = new NoteDTO(title, content, userId);
+
+            Assert.AreEqual(note.Title, title);
+        }
+
+        [Test]
+        public void SetContentCorrectly()
+        {
+            var content = "content";
+            var title = "title";
+            var userId = "id";
+            var note = new NoteDTO(title, content, userId);
+
+            Assert.AreEqual(note.Content, content);
+        }
+
+        [Test]
+        public void SetUserIdCorrectly()
+        {
+            var content = "content";
+            var title = "title";
+            var userId = "id";
+            var note = new NoteDTO(title, content, userId);
+
+            Assert.AreEqual(note.UserId, userId);
+        }
+    }
+}
---
+++ TeamToolsExtended/TeamTools.Logic.Tests/DTO/ProjectTaskDtoConstructor_Should.cs
@@ -0,0 +1,128 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TeamTools.Logic.Data.Models.Enums;
+using TeamTools.Logic.DTO;
+
+namespace TeamTools.Logic.Tests.DTO
+{
+    [TestFixture]
+    public class ProjectTaskDtoConstructor_Should
+    {
+        [Test]
+        public void CreateAnObjextOfTheSameTypeWithParameters()
+        {
+            string title = "title";
+            string description = "description";
+            DateTime time = DateTime.Now;
+            decimal cost = 5;
+            TaskType type = TaskType.Done;
+            int projectId = 5;
+
+            var taskDto = new ProjectTaskDTO(title, description, time, cost, type, projectId);
+
+            Assert.IsInstanceOf<ProjectTaskDTO>(taskDto);
+        }
+
+        [Test]
+        public void CreateAnObjextOfTheSameTypeWithoutParameters()
+        {
+            var taskDto = new ProjectTaskDTO();
+            Assert.IsInstanceOf<ProjectTaskDTO>(taskDto);
+        }
+
+        [Test]
+        public void SetTitleCorrectly()
+        {
+            string title = "title";
+            string description = "description";
+            DateTime time = DateTime.Now;
+            decimal cost = 5;
+            TaskType type = TaskType.Done;
+            int projectId = 5;
+
+            var taskDto = new ProjectTaskDTO(title, description, time, cost, type, projectId);
+
+            Assert.AreEqual(title, taskDto.Title);
+        }
+
+        [Test]
+        public void SetDescriptionCorrectly()
+        {
+            string title = "title";
+            string description = "description";
+            DateTime time = DateTime.Now;
+            decimal cost = 5;
+            TaskType type = TaskType.Done;
+            int projectId = 5;
+
+            var taskDto = new ProjectTaskDTO(title, description, time, cost, type, projectId);
+
+            Assert.AreEqual(description, taskDto.Description);
+        }
+
+        [Test]
+        public void SetTimeCorrectly()
+        {
+            string title = "title";
+            string description = "description";
+            DateTime time = DateTime.Now;
+            decimal cost = 5;
+            TaskType type = TaskType.Done;
+            int projectId = 5;
+
+            var taskDto = new ProjectTaskDTO(title, description, time, cost, type, projectId);
+
+            Assert.AreEqual(time, taskDto.DueDate);
+        }
+
+        [Test]
+        public void SetCostCorrectly()
+        {
+            string title = "title";
+            string description = "description";
+            DateTime time = DateTime.Now;
+            decimal cost = 5;
+            TaskType type = TaskType.Done;
+            int projectId = 5;
+
+            var taskDto = new ProjectTaskDTO(title, description, time, cost, type, projectId);
+
+            Assert.AreEqual(cost, taskDto.ExecutionCost);
+        }
+
+        [Test]
+        public void SetTaskTypeCorrectly()
+        {
+            string title = "title";
+            string description = "description";
+            DateTime time = DateTime.Now;
+            decimal cost = 5;
+            TaskType type = TaskType.Done;
+            int projectId = 5;
+
+            var taskDto = new ProjectTaskDTO(title, description, time, cost, type, projectId);
+
+            Assert.AreEqual(type, taskDto.Status);
+        }
+
+        [Test]
+        public void SetProjectIdCorrectly()
+        {
+            string title = "title";
+            string description = "description";
+            DateTime time = DateTime.Now;
+            decimal cost = 5;
+            TaskType type = TaskType.Done;
+            int projectId = 5;
+
+            var taskDto = new ProjectTaskDTO(title, description, time, cost, type, projectId);
+
+            Assert.AreEqual(projectId, taskDto.ProjectId);
+        }
+
+    }
+}
